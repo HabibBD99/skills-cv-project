474d16ac268e5062c9ebd79b8c422b120e7b78cb
Habib <70913270+HabibBD99@users.noreply.github.com>
2025-08-26 18:02:41 +0600

    cv
    js added

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+
+/*-----------------------------
+       Animation loader
+ ------------------------------*/ 
+     setTimeout(() => {
+      document.querySelector('.Animation_Card').style.display = 'none';
+      document.querySelector('.Main_Container').style.display = 'block';
+      document.querySelector('.Body').style.backgroundColor = 'turquoise';
+     //  document.querySelector('Heading').style.display = 'block';
+    }, 6000);
